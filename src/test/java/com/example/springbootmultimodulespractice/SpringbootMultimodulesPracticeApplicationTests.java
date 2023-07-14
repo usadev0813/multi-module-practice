9ef6b3883641659be4254bbe0bd7cf4f2ed09745
jeongho <tjsrsdqq4@gmail.com>
2023-07-14 17:09:31 +0900

    init

---
+++ src/test/java/com/example/springbootmultimodulespractice/SpringbootMultimodulesPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootmultimodulespractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMultimodulesPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
